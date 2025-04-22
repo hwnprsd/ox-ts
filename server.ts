fa32718f3ffe1bdbfcd317ff11debc9f92607985
ashwinprasad <d3fkonmusic@gmail.com>
2025-04-22 19:23:06 +0530

    clean

---
+++ server.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { ethers } from "ethers";
+dotenv.config();
+
+const privateKey = process.env.PRIVATE_KEY;
+const rpcUrl = process.env.RPC_URL;
+
+const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+const wallet = new ethers.Wallet(privateKey!, provider);
+
+const app = express();
+const port = 1337;
+
+app.get("/cosign", (req: Request, res: Response) => {
+  const hash = req.body.cosignerHash;
+
+  const signature = ethers.utils.joinSignature(
+    wallet._signingKey().signDigest(hash),
+  );
+  res.send(signature);
+});
+
+app.listen(port, () => {
+  console.log(`OX Cosigner listening on port ${port}`);
+});
